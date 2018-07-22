bb6ddfd1b9fa17c3144030ec9c174c8fa70c3b89
nykimberly <kimberlyvnguyen@gmail.com>
2018-07-21 22:26:06 -0700

    leetCode rotateArray problem * method 1: typical list.copy, for loop with correct index reassignments ** O(n), O(n) * method 2: add one 'swing' space at end of list and move items one at time ** O(n^2), O(1) * method 3: partition list in place and swap around k % n ** O(n), 0(1)

---
+++ Ad-hoc/rotateArray/solution.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    # O(n) space solution
+    """
+    def rotate(self, nums, k):
+        temp = list(nums)
+        length = len(nums)
+        for i, e in enumerate(temp):
+            n = (i + k) % length
+            nums[n] = e
+    """
+    # O(c) space solution
+    """
+    def rotate(self, nums, k):
+        og_last = len(nums) - 1
+        nums.append(nums[og_last])
+        for r in range(k):
+            for i in range(og_last + 1, 0, -1):
+                nums[i] = nums[i-1]
+            nums[0] = nums[og_last + 1]
+        nums.pop()
+    """
+    # O(1) space solution
+    def rotate(self, nums, k):
+        # length of list
+        n = len(nums)
+        # ensures k is within length of list
+        k = k % n
+        # partition list around k and swap first half with second half
+        nums[:] = nums[n-k:] + nums[:n-k]
