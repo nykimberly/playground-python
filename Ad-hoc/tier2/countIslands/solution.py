200307ff04a26006424f1c50183cbbe4f1b3f1af
nykimberly <kimberlyvnguyen@gmail.com>
2018-07-25 23:54:27 -0700

    leetcode dfs countislands medium problem

--- Ad-hoc/tier2/countIslands/solution.py
+++ Ad-hoc/tier2/countIslands/solution.py
@@ -1,12 +1,27 @@
 class Solution:
     def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
-        for i in range(1, len(grid)-1):
-            for j in range(1, len(grid[0])-1):
-                if grid[i][j] != grid[i+1][j]
-                    grid[i][j] != grid[i-1][j]
-                    grid[i][j] != grid[i][j+1]
-                    grid[i][j] != grid[i][j-1]
+
+        count = 0
+
+        # count the number of times we need to run dfs
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == "1":
+                    self.dfs(grid, i, j)
+                    count += 1
+        return count
+
+    def dfs(self, grid, i, j):
+
+        # travel only on islands
+        if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != "1":
+            return
+
+        # mark where we walk
+        grid[i][j] = "#"
+
+        # recurse
+        self.dfs(grid, i+1, j)
+        self.dfs(grid, i-1, j)
+        self.dfs(grid, i, j+1)
+        self.dfs(grid, i, j-1)
