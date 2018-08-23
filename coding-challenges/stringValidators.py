53fdcaf4ab4066d91a9c54562665041977b76cf1
nykimberly <kimberlyvnguyen@gmail.com>
2018-08-22 19:28:34 -0700

    print boolean of whether input is alpha/alphanum/num/upper/lower

---
+++ coding-challenges/stringValidators.py
@@ -0,0 +1,20 @@
+"""
+You are given a string s. 
+Your task is to find out if the string  contains: alphanumeric characters, alphabetical characters, digits, lowercase and uppercase characters.
+
+In the first line, print True if  has any alphanumeric characters. Otherwise, print False. 
+In the second line, print True if  has any alphabetical characters. Otherwise, print False. 
+In the third line, print True if  has any digits. Otherwise, print False. 
+In the fourth line, print True if  has any lowercase characters. Otherwise, print False. 
+In the fifth line, print True if  has any uppercase characters. Otherwise, print False.
+"""
+
+if __name__ == '__main__':
+    
+    s = input()   
+    
+    print(any(c.isalnum() for c in s))
+    print(any(c.isalpha() for c in s))
+    print(any(c.isdigit() for c in s))
+    print(any(c.islower() for c in s))
+    print(any(c.isupper() for c in s))
