b294b6cacf84b739bdb9c722dec37e520d397fd6
Kimberly Van Anh Nguyen <kimberlyvnguyen@gmail.com>
2018-09-23 11:00:10 -0700

    class vs. instance attr's, 'self', class methods

---
+++ hb/w2/d1_classes/classes-i.py
@@ -0,0 +1,54 @@
+#!/Users/kimberlyvnguyen/anaconda3/bin/python3
+
+class Animal:
+    """A living creature"""
+    nicknames = []
+# Can assign attributes to instances of class
+dog = Animal()
+dog.name = 'fido'
+print(dog.name)
+
+# In the below code, we’re setting nicknames = [] as a class attribute (set directly on the class). This means that all of the instances of Animal will share this list. So, if you made two Animals:
+fido = Animal()
+snowball = Animal()
+fido.nicknames.append("Woofster")
+print(fido.nicknames)
+print(snowball.nicknames)
+# You can prevent this problem by rebinding the nickname for Fido, rather than appending it:
+fido = Animal()
+snowball = Animal()
+fido.nicknames = ['Woofster']
+print(fido.nicknames)
+print(snowball.nicknames)
+# But a better suggestion is to not set mutable attributes as class attributes; always set these in the __init__ method described below. Many developers (including Hackbright staff) follow the even-better principle of setting all initial values in __init__, where possible, and setting nothing as class attributes. This can reduce confusion.
+
+# Another curious thing is making sure we use 'self' to refer to the instance
+# and className to refer to 'global' variable adjustments
+class CountKeeper:
+    count = 0
+    def add(self, value):
+        CountKeeper.count += value
+        # self.count += value here if you only wanted to change for instance
+x = CountKeeper()
+y = CountKeeper()
+x.add(3)
+print(x.count) # 3
+print(y.count) # 3
+
+# getattr()
+# print(fido.tail_length)
+    # ---------------------------------------------------------------------------
+    # AttributeError                            Traceback (most recent call last)
+    # <ipython-input-3-30bc9705613c> in <module>()
+    # ----> 1 fido.tail_length
+    # AttributeError: 'Animal' object has no attribute 'tail_length'
+getattr(fido, 'tail_length', 'unknown')
+    # 'unknown'
+
+# Functions Versus Methods
+    # Both are code
+    # Both have lists of parameters
+    # Both get called with ()
+    # Methods are defined on a class
+    # Methods take self as first argument
+    # When you call a method, Python passes instance as self
