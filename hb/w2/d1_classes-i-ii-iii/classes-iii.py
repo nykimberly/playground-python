12f42685aa0b1950a48448ba77764618282f2cbc
Kimberly Van Anh Nguyen <kimberlyvnguyen@gmail.com>
2018-09-24 13:49:03 -0700

    notes on mixins, static methods, class methods, exception classes, special methods

--- hb/w2/d1_classes-i-ii-iii/classes-iii.py
+++ hb/w2/d1_classes-i-ii-iii/classes-iii.py
@@ -101,4 +101,90 @@ class HairlessRobot(ChaseLaserPointerMixin, Animal):
 
     # ...
 
+# Mixins should come first in MRO as opposed to parent class
+
+# static method to add methods required for a class but not directly related
+
+class Cat:
+
+    # Methods that operate on class, not an instance
+    @classmethod
+    def get_from_db(cls, name):
+        """Get cat with given name from database."""
+
+        # # raise Exception("No such cat: {}".format(name))
+        # try:
+        #     cursor = db.session.execute(cls._SELECT, {'name': name})
+        #     cat_db_row = cursor.fetchone()
+        #     hunger = cat_db_row[1]   # row is (name, hunger)
+        # except LookupError:
+        #     print("I couldn't get your cat.")
+
+        cursor = db.session.execute(cls._SELECT, {'name': name})
+        cat_db_row = cursor.fetchone()
+        hunger = cat_db_row[1]   # row is (name, hunger)
+
+        # Better yet, extend exception class with our exception
+        if cat_db_row:
+            return cls(name, hunger)
+        else:
+            raise NoSuchCatError("No such cat: {}".format(name))
+
+    @staticmethod
+    def kibble_to_calories(kibble):
+        return kibble * 0.3456
+
+Cat.kibble_to_calories(10)
+
+# Look up 'decorators'
+
+class NoSuchCatError(Exception):
+    """Couldn't find this cat in database."""
+
+# Something using your Cat class, v4
+try:
+    snowball_ii = Cat.get_from_db("Snowball II")
+
+except NoSuchCatError:
+    print("I couldn't get your cat.")
+
+
+# Animal class with static factory method
+# This is because Animal is hard coded into the static method. A class method does the right thing.
+
+class Animal:
+    """Animal."""
+
+    @classmethod
+    def make_one(cls):
+        """Make an instance."""
+        return cls()
+
+class Cat:
+    """Cat."""
+
+>>> Animal.make_one()
+    <__main__.Animal object at 0x10ba3e110>
+
+>>> Cat.make_one()
+    <__main__.Cat object at 0x10ba63d50>
+
+
+# define a repr to show more useful object info
+
+Special methods
+__repr__
+<__main__.Cat object at 0x10ba63d50> sucks. How useless is this?
+
+class Cat:
+     def __repr__(self):
+         return f"<Cat: name={self.name}, hunger={self.hunger}>"
+
+>>> auden = Cat('Auden')
+>>> print(auden)
+<Cat: name=Auden, hunger=50>
+Other special methods:
+__lt__, __le__, __gt__, __ge__, __eq__
+__bool__
+__hash__
 
