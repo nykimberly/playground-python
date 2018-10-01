ee9f1e61d3e17fb8fcbd7ce49322d83eaca8db8e
Kimberly Van Anh Nguyen <kimberlyvnguyen@gmail.com>
2018-10-01 15:36:32 -0700

    eq method to compare dictionaries and return false if not equivalent

--- hb/w3/d1_homemade-dict-review/homemadehash.py
+++ hb/w3/d1_homemade-dict-review/homemadehash.py
@@ -48,6 +48,30 @@ class HomemadeHash:
         self._items[slot] = None
         self._used_slots -= 1
 
+    def __eq__(self, other):
+        """Returns value with key if it exists or keyerror otherwise"""
+        # eliminate incorrectly sized lists right away
+        if len(self) != len(other):
+            return False
+
+        # now compare keys using set operations
+        self_keys = set(self.keys())
+        other_keys = set(other.keys())
+
+        if len(self_keys ^ other_keys) != 0:
+            return False
+
+        for key in self_keys:
+            if self[key] != other[key]:
+                return False
+
+        return True
+
+    def keys(self):
+        """Return list of keys in dictionary"""
+        return [item[0] for item in self._items if item is not None]
+
+
 class SlotInUseError(ValueError):
     """Error if hashed slot is already in use"""
 
