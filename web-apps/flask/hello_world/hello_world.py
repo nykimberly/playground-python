cbba9f4f290147d41522d9f4fc9b74c530e5a0cd
n-kimberly <kimberlyvnguyen@gmail.com>
2021-05-31 18:38:11 -0700

    variables in flask app path

--- web-apps/flask/hello_world/hello_world.py
+++ web-apps/flask/hello_world/hello_world.py
@@ -3,8 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def hello_world():
     return "Hello World!"
+
+@app.route("/hello/<name>")
+def hello_name(name):
+    return "Hello %s" % name
 
 if __name__ == "__main__":
     app.run()
