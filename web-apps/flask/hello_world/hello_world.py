d6413f54e41e21c36610d0d089651316226e4b5c
n-kimberly <kimberlyvnguyen@gmail.com>
2021-06-03 12:09:11 -0700

    more examples of endpoints for hello world flask

--- web-apps/flask/hello_world/hello_world.py
+++ web-apps/flask/hello_world/hello_world.py
@@ -1,4 +1,9 @@
-from flask import Flask
+import json
+import time
+
+from flask import Flask, request
+from typing import Dict, List, Any
+
 
 app = Flask(__name__)
 
@@ -6,9 +11,43 @@ app = Flask(__name__)
 def hello_world():
     return "Hello World!"
 
-@app.route("/hello/<name>")
-def hello_name(name):
-    return "Hello %s" % name
+chatroom_users = {
+    "chat": set()
+}
+default_chatroom_msg = [
+        {
+            "author": "chat_help", 
+            "msg":"hello, welcome!",
+            "timestamp": "0"
+        }
+    ]
+chatroom_msgs: Dict[str, List[Dict[str, Any]]] = {
+    "chat": default_chatroom_msg
+}
+
+@app.route("/join/<chatroom>/<name>")
+def join_chatroom_name(chatroom, name):
+    chatroom_users[chatroom].add(name)
+    users =  chatroom_users[chatroom]
+    return f"welcome to {chatroom}, {len(users)} are here: {users}"
+
+@app.route("/view/<chatroom>")
+def view_chatroom(chatroom):
+    if not chatroom_msgs.get(chatroom):
+        chatroom_msgs[chatroom] = default_chatroom_msg
+    return json.dumps(chatroom_msgs.get(chatroom))
+
+@app.route("/post/<chatroom>/<name>", methods = ["POST", "GET"])
+def post_chatroom(chatroom, name):
+    if chatroom in chatroom_msgs:
+        chatroom_msgs[chatroom].append(
+            {
+                "author": name,
+                "msg": request.form.get("msg"),
+                "timestamp": time.time()
+            }
+        )
+    return json.dumps(chatroom_msgs.get(chatroom))
 
 if __name__ == "__main__":
     app.run()
