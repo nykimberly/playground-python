c086bc164eac3e79d2a163bf080cca6523713c3a
n-kimberly <kimberlyvnguyen@gmail.com>
2021-06-10 21:24:25 -0700

    working palindrome pairs solution (w/ trie)

--- coding-challenges/palindrome_pairs.py
+++ coding-challenges/palindrome_pairs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from collections import defaultdict
 
 """
 Given a list of unique words, find all pairs of distinct indices (i, j) in the given list, so that the concatenation of the two words, i.e. words[i] + words[j] is a palindrome.
@@ -14,27 +15,59 @@ def is_palindrome(s):
             return False
     return True
 
-def palindrome_pairs_failed(list_words):
-    """This implementation does not work.
-    It is possible that we need to index both sll and s to key s.
-    We will need a trie.
-    """
+
+class Trie:
+    def __init__(self):
+        self.paths = defaultdict(Trie)
+        self.end_idx = -1
+        self.word = ""
+        self.palindromes_below = []
+    
+    def add_word(self, word, idx):
+        trie = self
+        for j, char in enumerate(reversed(word)):
+            if is_palindrome(word[:len(word) - j]):
+                trie.palindromes_below.append(word[:len(word) - j])
+            trie = trie.paths[char]
+        trie.end_idx = idx
+        trie.word = word
+
+
+def make_trie(words):
+    trie = Trie()
+    for i, word in enumerate(words):
+        trie.add_word(word, i)
+    return trie
+
+
+def get_palindromes(trie, word):
+    palindromes = []
+    curr = word
+    while curr:
+        if trie.end_idx >= 0:
+            if not curr[0] in trie.paths:
+                return palindromes
+        trie = trie.paths[curr[0]]
+        curr = curr[1:]
+    if trie.end_idx >= 0:
+        palindromes.append(trie.word)
+    if trie.palindromes_below:
+        palindromes.extend(
+            [postfix + word[::-1] for postfix in trie.palindromes_below]
+        )
+    return palindromes
+
+
+def palindrome_pairs(words):
+    trie = make_trie(words)
     pairs = []
-    fwd_ss = {}
-    rev_ss = {}
-    for word in list_words:
-        for i in range(1, len(word) + 1):
-            pre, rev_pre = word[:i], word[::-1][:i]
-            post, rev_post = word[i:], word[::-1][i:]
-            pre_rev = pre[::-1]
-            if is_palindrome(post):
-                if pre_rev in fwd_ss:
-                    pairs.append([pre_rev + fwd_ss[pre_rev], pre + post])
-                fwd_ss[pre] = post
-            if is_palindrome(rev_post):
-                if pre_rev in rev_ss:
-                    pairs.append([pre + post, pre_rev + fwd_ss[pre_rev]])
-                rev_ss[rev_pre] = rev_post
+    for word in words:
+        candidates = get_palindromes(trie, word)
+        pairs.extend([
+            [word, pair]
+            for pair in candidates 
+            if pair and pair != word
+        ])
     return pairs
 
 
@@ -42,4 +75,18 @@ if __name__ == "__main__":
     assert is_palindrome("") == True
     assert is_palindrome("ababa") == True
     assert is_palindrome("ab") == False
-    assert palindrome_pairs_failed(["lls","s","sssll"]) == [["s", "lls"], ["lls", "sssll"]]
+    inputs = [
+        ["abcd","dcba","lls","s","sssll"],
+        ["bat","tab","cat"],
+        ["run", "nu"]
+    ]
+    expected = [
+        [["abcd", "dcba"], ["dcba", "abcd"], ["s", "lls"], ["lls", "sssll"]],
+        [["bat", "tab"], ["tab", "bat"]],
+        [["nu", "run"]]
+    ]
+    for i, item in enumerate(inputs):
+        output = palindrome_pairs(item)
+        assert len(output) == len(expected[i])
+        assert [item in output for item in expected[i]]
+    print("success!")
