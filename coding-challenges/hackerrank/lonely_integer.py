b50a7d726d17215ab5a1fc39eb68c591b87906bd
nykimberly <kimberlyvnguyen@gmail.com>
2021-11-10 08:57:44 -0800

    add tests for lonely_integer

--- coding-challenges/hackerrank/lonely_integer.py
+++ coding-challenges/hackerrank/lonely_integer.py
@@ -9,24 +9,40 @@ int: the element that occurs only once
 Approach: Frequency map and return the one with count of 1
 """
 
-def lonelyinteger(a):
-    counter = {}
-    for item in a:
-        counter.setdefault(item, 0)
-        counter[item] += 1
-    for item, count in counter.items():
-        if count == 1:
-            return item
+import collections
+import unittest
+import typing
 
 
-def lonelyinteger2(a):
-    arr = collections.Counter(a)
-    for k, v in arr.items():
-        if v == 1:
-            return k
+class TestLonelyInteger(unittest.TestCase):
+    def test_lonely_integer_fns(self):
+        lonely_integer = LonelyInteger()
 
-        
-if __name__ == '__main__':
-    a = [4, 9, 95, 93, 57, 4, 57, 93, 9]
-    result = lonelyinteger(a)
-    print(result)
+        def test_lonely_integer(fn: typing.Callable) -> None:
+            expected = {(4, 9, 95, 93, 57, 4, 57, 93, 9): 95, (1, 1, 2): 2, (1,): 1}
+            for i, expected in expected.items():
+                arr = list(i)
+                self.assertEqual(expected, fn(arr))
+
+        for fn in lonely_integer.fns:
+            test_lonely_integer(fn)
+
+
+class LonelyInteger:
+    def __init__(self) -> None:
+        self.fns = [self.lonely_integer, self.lonely_integer_with_counter]
+
+    def lonely_integer(self, arr: typing.List[int]) -> None:
+        counter = {}
+        for item in arr:
+            counter.setdefault(item, 0)
+            counter[item] += 1
+        for item, count in counter.items():
+            if count == 1:
+                return item
+
+    def lonely_integer_with_counter(self, arr: typing.List[int]) -> None:
+        counter = collections.Counter(arr)
+        for k, v in counter.items():
+            if v == 1:
+                return k
